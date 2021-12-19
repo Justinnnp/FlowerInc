f2bff714df98c1af35996ff9ee2e0efb8448d89e
Justinnnp <plein.justin@gmail.com>
2021-12-19 21:45:39 +0100

    The web.php file changed.

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-//    ->middleware(['auth:sanctum', 'verified']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
@@ -22,9 +21,6 @@ Route::get('/flowers', [FlowerController::class, 'index'])->name('flowers.index'
 
 Route::get('/flowers/{flower}', [FlowerController::class, 'show'])->name('flowers.show')->middleware(['auth:sanctum', 'verified']);
 
-
-//stocks
-
 Route::get('/stocks/{stock}/flowers/create', [FlowerController::class, 'create'])->name('flowers.create')->middleware(['auth:sanctum', 'verified']);
 
 Route::post('/stocks/{stock}/flowers/store', [FlowerController::class, 'store'])->name('flowers.store')->middleware(['auth:sanctum', 'verified']);
@@ -35,4 +31,3 @@ Route::post('/stocks/{stock}/flowers/{flower}/update', [FlowerController::class,
 
 Route::delete('/stocks/{stock}/flowers/{flower}/delete', [FlowerController::class, 'destroy'])->name('flowers.destroy')->middleware(['auth:sanctum', 'verified']);
 
-//Route::resource('flowers', FlowerController::class)->middleware(['auth:sanctum', 'verified']);
